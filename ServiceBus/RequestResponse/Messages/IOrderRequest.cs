e73eaa775bfc89111a25996514ad3560bca0df8d
Kornel Warwas <kornel.warwas@iteo.com>
2019-11-22 21:21:43 +0100

    MassTransit

---
+++ ServiceBus/RequestResponse/Messages/IOrderRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Messages
+{
+    public interface IOrderRequest
+    {
+        string Name { get; set; }
+        double Weight { get; set; }
+    }
+}
---
+++ ServiceBus/RequestResponse/Receiver/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using Messages;
+using Serilog;
+using Serilog.Events;
+
+namespace Receiver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost/"), hostCfg =>
+                {
+                    hostCfg.Username("guest");
+                    hostCfg.Password("guest");
+                });
+
+                cfg.UseSerilog();
+
+                cfg.ReceiveEndpoint(host, "order-req-res", e =>
+                {
+                    e.Handler<IOrderRequest>(async x =>
+                        {
+                            await Console.Out.WriteLineAsync($"Handler: {x.Message.Name} {DateTime.Now}");
+
+                            await x.RespondAsync<IOrderResponse>(new
+                            {
+                                x.Message.Name,
+                                HandledDate = DateTime.Now
+                            });
+                        }
+                    );
+                });
+            });
+
+            await bus.StartAsync();
+
+            Console.WriteLine("Bus started");
+
+            Console.ReadLine();
+
+            await bus.StopAsync();
+
+            Console.WriteLine("Bus stopped");
+        }
+    }
+}
